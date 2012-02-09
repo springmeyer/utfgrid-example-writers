c8ade65fa97f8de698abc5af30f82a69d044a9f6
Matthew Perry <mperry@ecotrust.org>
2012-02-09 10:37:06 -0800

    Time the renderers

--- run_all.py
+++ run_all.py
@@ -1,5 +1,6 @@
 import glob
 import subprocess
+from datetime import datetime
 try:
     import json
 except ImportError:
@@ -7,14 +8,22 @@ except ImportError:
 
 grid_cols = []
 title_cols = []
+time_cols = []
 for f in glob.glob("*_renderer.py"):
     print f
+    start = datetime.now()
     proc = subprocess.Popen(['python',f], shell=False, stdout=subprocess.PIPE)
+    proc.wait()
+    end = datetime.now() 
+    totaltime = (end - start)
+    elapsed = totaltime.seconds + totaltime.microseconds/1000000.0
+    print elapsed
     j = json.loads(proc.communicate()[0])
     grid = j['grid']
     grid_txt = '\n'.join([str(row) for row in grid])
     grid_cols.append("<td><pre>%s</pre></td>" % grid_txt)
     title_cols.append("<td>%s</td>" % f)
+    time_cols.append("<td>%s sec</td>" % elapsed)
 
 ofh = open("output.html",'w')
 ofh.write("""<html>
@@ -28,7 +37,11 @@ ofh.write("""<html>
 <table>
 <tr>%s</tr>
 <tr>%s</tr>
+<tr>%s</tr>
 </table>
 </body>
-</html>""" % ('\n'.join(title_cols), '\n'.join(grid_cols)))
+</html>""" % ('\n'.join(title_cols), 
+              '\n'.join(time_cols),
+              '\n'.join(grid_cols)
+))
 ofh.close()
